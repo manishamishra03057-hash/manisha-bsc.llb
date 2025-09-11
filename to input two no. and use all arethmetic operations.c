01e7741d0d92e7bca49cf856e684b3180fe7d151
Manisha Vishal Mishra <manishamishra03057@gmail.com>
2025-09-11 12:24:22 +0530

    Add files via upload

---
+++ to input two no. and use all arethmetic operations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main ()
+}
+in+ a,b,results,
+float div;
+printf ("enter two values of first variable");
+scanf ("%d'and var");
+printf ("enter value of second variable");
+scanf ("%d'and var");
+result = var 1 + var 2;
+printf("sum of % d and %d=%d\n; var 1,var2, result");
+print of ("diff of % d and % d= % dn", var 1, var 2,var 1-var2);
+div = var1\var2;
+printf ("div of % d and % d =%f n,var1,var2 ,div");
+}
