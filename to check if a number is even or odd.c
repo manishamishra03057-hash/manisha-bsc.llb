43a66395a6fb7ae086548d58995ecfd1e7bff8d0
Manisha Vishal Mishra <manishamishra03057@gmail.com>
2025-09-24 16:21:28 +0530

    Add files via upload

---
+++ to check if a number is even or odd.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+main void ()
+{
+int x;
+print ("enter value of x");
+scan f (% d" ,& x);
+if (% 2==0);
+}
+ print( " x is even ");
+ {
+     else
+ }
+ print f (" x is  odd ");
+ {
+ }
+
