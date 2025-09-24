72b3cd9316955ad149ef16743be12b8c0019ac51
Manisha Vishal Mishra <manishamishra03057@gmail.com>
2025-09-24 15:33:19 +0530

    Add files via upload

---
+++ to calculate area & perimeter of a rectangle.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+void main ()
+{
+float length , width,area , perimeter
+printf ("enter length");
+scanf ("%f & length");
+printf ("enter width");
+scanf ("%f & width");
+area = length * width ;
+perimeter = 2*(length+ width );
+print f ("area % 2* (l+w)");
+print f ( "perimeter % 2 f/n,perimeter ");
+}
