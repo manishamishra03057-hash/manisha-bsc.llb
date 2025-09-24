2ae4f67c4a1b212a3e2748f1d4a8a5ce8958fe5f
Manisha Vishal Mishra <manishamishra03057@gmail.com>
2025-09-24 15:44:18 +0530

    Add files via upload

---
+++ to convert crlcius into farenheit.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+void main ()
+{
+    float,celcius, fahrenheit,
+    printf ("enter temperature in celcius ");
+    scanf (% & celcius)
+    fahrenheit= ( celcius * 9.0)
+    printf ("the temperature n 5.0)+32.0
+    far is % f/n", fahrenheit)
+}
